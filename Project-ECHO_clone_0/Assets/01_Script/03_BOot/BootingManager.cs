f4e6dfee3a89e33ec255a1cdbf4d799966390d50
KChangHoon <dkskspd2@Naver.com>
2026-01-07 23:55:27 +0900

    asset collect

---
+++ Project-ECHO_clone_0/Assets/01_Script/03_BOot/BootingManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class BootingManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        SceneManager.LoadScene("Lobby");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
